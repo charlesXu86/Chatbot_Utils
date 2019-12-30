61ea851baabc8ac30823021da85c1732d580d966
Xu <charlesxu86@163.com>
2019-12-30 11:09:58 +0800

    Initial commit

---
+++ Base_model/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+'''
+@Author  :   Xu
+ 
+@Software:   PyCharm
+ 
+@File    :   __init__.py.py
+ 
+@Time    :   2019-12-30 11:01
+ 
+@Desc    :
+ 
+'''
---
+++ Base_model_rest/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class BaseModelRestConfig(AppConfig):
+    name = 'Base_model_rest'
